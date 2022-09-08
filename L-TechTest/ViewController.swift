ccd8fcac19d5e6ab2e0af9baa8d96343f8632c1a
Evgeny Ganusenko <evgan66@gmail.com>
2022-09-08 14:02:25 +0300

    Initial Commit

---
+++ L-TechTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  L-TechTest
+//
+//  Created by Евгений Ганусенко on 9/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
