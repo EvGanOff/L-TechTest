e7fb9ee5c31cd8c6839b32e72d606ef92b4d6295
Evgeny Ganusenko <evgan66@gmail.com>
2022-09-13 11:37:12 +0300

    bugFix

--- L-TechTest/Source/MainModels/MainModel.swift
+++ L-TechTest/Source/MainModels/MainModel.swift
@@ -13,12 +13,13 @@ struct MainModel: Decodable {
     var image: String?
     var sort: Int?
     var date: Date?
+    private let prefixURL = "http://dev-exam.l-tech.ru"
 
     init(mainModel: MainModel) {
         self.id = mainModel.id
         self.title = mainModel.title
         self.text = mainModel.text
-        self.image = "http://dev-exam.l-tech.ru\(mainModel.image ?? "")"
+        self.image = "\(prefixURL)"+"\(mainModel.image ?? "")"
         self.sort = mainModel.sort
         self.date = mainModel.date
     }
